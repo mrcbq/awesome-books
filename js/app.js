6d507d543e927ea9e72150efa261e14fb801333e
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-24 12:27:50 -0500

    Local Storage Setup

--- js/app.js
+++ js/app.js
@@ -14,3 +14,11 @@ Books.map((obj) => {
     <button>Remove</button>
     `;
 });
+
+//LOCAL STORAGE
+
+const formValidator = document.getElementById('form-add_book');
+const bTitle = document.getElementById('title')
+const bAuthor = document.getElementById('author')
+const dataBooks = localStorage.getItem('');
+const localStorage = JSON.parse(dataBooks);
