abbd52440debe4da1abe7ac2b0d544503181b3e1
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-24 14:56:55 -0500

    Update app.js
    Created function for:
    - Add a book
    - Remove a book
    - Preserve data locally
    
    Co-Authored-By: Marco Baquero <83478996+mrcbq@users.noreply.github.com>

--- js/app.js
+++ js/app.js
@@ -1,24 +1,68 @@
-const bookList = document.querySelector('.book-list')
+let Books = [];
 
-const Books = [
-{
-    title: 'Libro1',
-    author: 'Autor1',
+function getInput(){
+    const book = {};
+    book.title = document.getElementById('book-title').value;
+    book.author = document.getElementById('book-author').value;
+    return book;
 }
-]
 
-Books.map((obj) => {
-    bookList.innerHTML += `
-    <p>${obj.title}</p>
-    <p>${obj.author}</p>
-    <button>Remove</button>
+function removeBook(title) {
+    const book = document.getElementById(title);
+    book.remove();
+    Books = Books.filter((bookObj) => bookObj.title !== title);
+    localStorage.setItem('Books', JSON.stringify(Books));
+}
+  
+  function addBook(bookObj) {
+    const bookList = document.getElementById('book-list');
+    const book = document.createElement('LI');
+    book.setAttribute('id', bookObj.title);
+    book.innerHTML = `
+    <h3> ${bookObj.title} </h3> 
+    <p>  ${bookObj.author} </p>
     `;
+    const removeBtn = document.createElement('button');
+    removeBtn.innerHTML = 'Remove';
+    removeBtn.addEventListener('click', () => removeBook(bookObj.title));
+    book.appendChild(removeBtn);
+    bookList.appendChild(book);
+}
+
+const addButton = document.getElementById('btn-add');
+addButton.addEventListener('click', () => {
+  const book = getInput();
+  Books.push(book);
+  localStorage.setItem('Books', JSON.stringify(Books));
+  addBook(book);
 });
+
+window.onload = () => {
+    Books = JSON.parse(localStorage.getItem('Books' || '[]'));
+    if (Books === null) {
+      Books = [];
+      return;
+    }
+  
+    Books.forEach((book) => {
+      addBook(book);
+    });
+  };
+
+
+
+// Books.map((obj) => {
+//     bookList.innerHTML += `
+//     <p>${obj.title}</p>
+//     <p>${obj.author}</p>
+//     <button>Remove</button>
+//     `;
+// });
 
 //LOCAL STORAGE
 
-const formValidator = document.getElementById('form-add_book');
-const bTitle = document.getElementById('title')
-const bAuthor = document.getElementById('author')
-const dataBooks = localStorage.getItem('');
-const localStorage = JSON.parse(dataBooks);
+// const formValidator = document.getElementById('form-add_book');
+// const bTitle = document.getElementById('title')
+// const bAuthor = document.getElementById('author')
+// const dataBooks = localStorage.getItem('');
+// const localStorage = JSON.parse(dataBooks);
