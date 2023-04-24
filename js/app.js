272c0a7bbbb2ba757290e555dd1b57d77a97994f
mrcbq <marcobaquero@gmail.com>
2023-04-24 17:01:38 -0500

    Fix Linters errors

--- js/app.js
+++ js/app.js
@@ -1,42 +1,42 @@
-let Books = JSON.parse(localStorage.getItem("Books" || "[]"));
-
-Books.forEach((book) => {
-  addBook(book);
-});
+let Books = JSON.parse(localStorage.getItem('Books' || '[]'));
 
 function removeBook(title) {
   const book = document.getElementById(title);
   book.remove();
   Books = Books.filter((bookObj) => bookObj.title !== title);
-  localStorage.setItem("Books", JSON.stringify(Books));
+  localStorage.setItem('Books', JSON.stringify(Books));
 }
 
 function addBook(bookObj) {
-  const bookList = document.getElementById("book-list");
-  const book = document.createElement("li");
-  book.setAttribute("id", bookObj.title);
+  const bookList = document.getElementById('book-list');
+  const book = document.createElement('li');
+  book.setAttribute('id', bookObj.title);
   book.innerHTML = `
-    <h3> ${bookObj.title} </h3> 
-    <p>  ${bookObj.author} </p>
-    `;
-  const removeBtn = document.createElement("button");
-  removeBtn.innerHTML = "Remove";
-  removeBtn.addEventListener("click", () => removeBook(bookObj.title));
+      <h3> ${bookObj.title} </h3> 
+      <p>  ${bookObj.author} </p>
+      `;
+  const removeBtn = document.createElement('button');
+  removeBtn.innerHTML = 'Remove';
+  removeBtn.addEventListener('click', () => removeBook(bookObj.title));
   book.appendChild(removeBtn);
   bookList.appendChild(book);
 }
 
-const form = document.querySelector("#form");
+Books.forEach((book) => {
+  addBook(book);
+});
 
-form.addEventListener("submit", (e) => {
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   const book = {};
-  book.title = document.getElementById("book-title").value;
-  book.author = document.getElementById("book-author").value;
+  book.title = document.getElementById('book-title').value;
+  book.author = document.getElementById('book-author').value;
 
-  if ((book.title && book.author) != "") {
+  if ((book.title && book.author) !== '') {
     Books.push(book);
-    localStorage.setItem("Books", JSON.stringify(Books));
+    localStorage.setItem('Books', JSON.stringify(Books));
     addBook(book);
   }
 });
