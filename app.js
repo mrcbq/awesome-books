265ae1a2fa35df6335106c78aec9e90cb4f83555
mrcbq <marcobaquero@gmail.com>
2023-04-25 16:29:34 -0500

    Fix Linters errors and Refactor Class end

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let Books = JSON.parse(localStorage.getItem('Books')) ?? [];
 
 class Book {
   constructor(title, author) {
-    this.id = Books.length;
+    this.id = Math.random() * 100;
     this.title = title;
     this.author = author;
   }
@@ -31,12 +31,11 @@ function displayBook() {
   const bookListDiv = document.querySelector('.book-list');
   bookListDiv.innerHTML = '';
   const bookList = document.createElement('ul');
-  Books.forEach((element) => {
+  Books.forEach((element, idx) => {
     const li = document.createElement('li');
     li.setAttribute('id', element.id);
     li.innerHTML = `
-    <h3> ${element.title} </h3> 
-    <p>  ${element.author} </p>
+    <p> "${element.title}" By ${element.author} </p> 
     `;
     const removeBtn = document.createElement('button');
     removeBtn.innerHTML = 'Remove';
@@ -44,8 +43,14 @@ function displayBook() {
     removeBtn.addEventListener('click', () => {
       library.removeBook(element);
       li.remove();
+      displayBook();
     });
     li.appendChild(removeBtn);
+    if (idx % 2 === 0) {
+      li.classList.add('dark');
+    } else {
+      li.classList.add('light');
+    }
     bookList.appendChild(li);
   });
   bookListDiv.appendChild(bookList);
