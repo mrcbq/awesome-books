65ae0fb06d005327e8e81fd9ad93fce2b9ff9731
mrcbq <marcobaquero@gmail.com>
2023-04-24 23:17:41 -0500

    Fix CR Comments and linters ok

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
-let Books = JSON.parse(localStorage.getItem('Books' || '[]'));
-if (Books === null) {
-  Books = [];
-}
+let Books = JSON.parse(localStorage.getItem('Books')) ?? [];
 
-function removeBook(title) {
-  const book = document.getElementById(title);
+function removeBook(id) {
+  const book = document.getElementById(id);
   book.remove();
-  Books = Books.filter((bookObj) => bookObj.title !== title);
+  Books = Books.filter((bookObj) => bookObj.id !== id);
   localStorage.setItem('Books', JSON.stringify(Books));
 }
 
-function addBook(bookObj) {
+function displayBook(id, title, author) {
   const bookList = document.getElementById('book-list');
-  const book = document.createElement('li');
-  book.setAttribute('id', bookObj.title);
-  book.innerHTML = `
-      <h3> ${bookObj.title} </h3> 
-      <p>  ${bookObj.author} </p>
+  const li = document.createElement('li');
+  li.setAttribute('id', id);
+  li.innerHTML = `
+      <h3> ${title} </h3> 
+      <p>  ${author} </p>
       `;
   const removeBtn = document.createElement('button');
   removeBtn.innerHTML = 'Remove';
-  removeBtn.addEventListener('click', () => removeBook(bookObj.title));
-  book.appendChild(removeBtn);
-  bookList.appendChild(book);
+  removeBtn.addEventListener('click', () => removeBook(id));
+  li.appendChild(removeBtn);
+  bookList.appendChild(li);
+}
+
+function addNewBook(title, author) {
+  const book = {};
+  book.id = Books.length;
+  book.title = title;
+  book.author = author;
+  Books.push(book);
+  localStorage.setItem('Books', JSON.stringify(Books));
+  return book.id;
 }
 
 Books.forEach((book) => {
-  addBook(book);
+  displayBook(book.id, book.title, book.author);
 });
 
 const form = document.querySelector('#form');
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const book = {};
-  book.title = document.getElementById('book-title').value;
-  book.author = document.getElementById('book-author').value;
-
-  if ((book.title && book.author) !== '') {
-    Books.push(book);
-    localStorage.setItem('Books', JSON.stringify(Books));
-    addBook(book);
+  const title = document.getElementById('book-title').value;
+  const author = document.getElementById('book-author').value;
+  if ((title && author) !== '') {
+    const id = addNewBook(title, author);
+    displayBook(id, title, author);
   }
 });
